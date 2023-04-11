7561911a60978953a6d56f6c3bc1ba9bee3f27d5
Folarin Lawal <lawalfolarin6@gmail.com>
2023-04-11 17:28:46 +0100

    fixed loader

--- pages/index.js
+++ pages/index.js
@@ -15,26 +15,13 @@ export default function Home() {
 
   
 
-  const [timer, setTimer] = useState(1);
-
-  const id = useRef(null);
-
-  const clear = () => {
-    window.clearInterval(id.current);
-    setPreloader(false);
-  };
+  
 
   useEffect(() => {
-    id.current = window.setInterval(() => {
-      setTimer((timer) => timer - 1);
-    }, 1000);
+    setTimeout(() => {
+      setPreloader(false);
+    }, 3000);
   }, []);
-
-  useEffect(() => {
-    if (timer === 0) {
-      clear();
-    }
-  }, [timer]);
 
   return (
     <>
